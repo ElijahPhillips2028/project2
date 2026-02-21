13f892af9077778e596977adc854b40846c6bcaf
Elijah Phillips <elp183@pitt.edu>
2026-02-21 14:31:26 -0500

    Starting Code For Palidromes

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/elilp/Downloads/project2/project2
+# Build directory: C:/Users/elilp/Downloads/project2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-FindPalindrome "test-FindPalindrome")
+set_tests_properties(test-FindPalindrome PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/elilp/Downloads/project2/project2/CMakeLists.txt;16;add_test;C:/Users/elilp/Downloads/project2/project2/CMakeLists.txt;0;")
